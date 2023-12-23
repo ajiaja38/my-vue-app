400d63b45d103e6fb40946df850acd77ade8981d
masundareza123 <ngekngek328@gmail.com>
2023-12-23 21:29:30 +0700

    my vue app

---
+++ src/components/navbar/NavbarCommon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full bg-green-400 py-3 px-5 shadow flex justify-between items-center">
+    <h1 class="text-white font-extrabold">{{ name }}</h1>
+    <ul class="text-white font-semibold flex gap-2">
+      <li>Home</li>
+      <li>About</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template clas>
+  <navbar name="Aji Perdana" />
+  <div class="px-4">
+    <h1>This is an home page</h1>
+    <button
+      @click="handleClick"
+      class="p-2 bg-green-400 hover:bg-green-300 active:bg-green-200 rounded"
+    >
+      Klik saya
+    </button>
+  </div>
+</template>
+
+<script>
+import NavbarCommonVue from '@/components/navbar/NavbarCommon.vue'
+
+export default {
+  components: {
+    navbar: NavbarCommonVue
+  },
+  methods: {
+    handleClick() {
+      alert('Tombol telah di klik')
+    }
+  }
+}
+</script>
